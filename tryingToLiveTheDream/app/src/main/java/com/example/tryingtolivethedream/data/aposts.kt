070c2af340d1c038bef34fd69ad22cf4a786ffee
alSheiye5h <anasbelkhadir13@gmail.com>
2025-02-28 23:58:35 +0000

    _

--- tryingToLiveTheDream/app/src/main/java/com/example/tryingtolivethedream/data/aposts.kt
+++ tryingToLiveTheDream/app/src/main/java/com/example/tryingtolivethedream/data/aposts.kt
@@ -1,2 +1,11 @@
 package com.example.tryingtolivethedream.data
 
+import com.example.tryingtolivethedream.PostItem
+import com.example.tryingtolivethedream.domain.Post
+
+var posts
+    get() = arrayOf(
+        Post() { }
+
+    )
+    set(value) = TODO()
