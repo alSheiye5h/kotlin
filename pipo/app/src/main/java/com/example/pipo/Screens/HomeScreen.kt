7223ae9e025a17fe132653ee237282e80e9c8d13
alSheiye5h <anasbelkhadir13@gmail.com>
2025-03-15 21:41:45 +0000

    _

--- pipo/app/src/main/java/com/example/pipo/Screens/HomeScreen.kt
+++ pipo/app/src/main/java/com/example/pipo/Screens/HomeScreen.kt
@@ -9,23 +9,36 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,12 +48,16 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavHostController
+import com.example.pipo.R
 
 @Composable
 fun HomeScreen(navController: NavHostController, innerPadding: PaddingValues) {
@@ -54,7 +71,10 @@ fun HomeScreen(navController: NavHostController, innerPadding: PaddingValues) {
         modifier = Modifier
             .fillMaxSize()
             .background(scaffoldBrush),
+        contentAlignment = Alignment.Center
     ) {
+        var showNewMessage by remember { mutableStateOf(false) }
+
         Scaffold (
             modifier = Modifier
                 .fillMaxSize()
@@ -73,7 +93,7 @@ fun HomeScreen(navController: NavHostController, innerPadding: PaddingValues) {
                     horizontalArrangement = Arrangement.End
                 ) {
                     val entireScreenWithDp = LocalConfiguration.current.screenWidthDp.dp
-                    NewMessage(Modifier.padding(bottom = LocalConfiguration.current.screenWidthDp.dp * 0.15f, end = 10.dp))
+                    NewMessage(showNewMessage = {if (showNewMessage == false) showNewMessage = true else showNewMessage = false }, Modifier.padding(bottom = LocalConfiguration.current.screenWidthDp.dp * 0.15f, end = 10.dp))
                 }
             }
         ) {innerPadding ->
@@ -83,14 +103,93 @@ fun HomeScreen(navController: NavHostController, innerPadding: PaddingValues) {
 
         }
 
+        SendNewMessage(showNewMessage, falseNewMsg = {showNewMessage = false})
+
     }
 }
 
 @Composable
-fun NewMessage(modifier: Modifier) {
+fun SendNewMessage(showNewMessage: Boolean, falseNewMsg: () -> Unit) {
+    if (showNewMessage) {
+        Dialog(onDismissRequest = { falseNewMsg() }) {
+            Card(
+                modifier = Modifier.fillMaxHeight(.8f).fillMaxWidth(.9f),
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Row (
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Start a New Conversation",
+                            style = TextStyle(
+                                color = Color.White,
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.SemiBold
+                            ),
+                            modifier = Modifier.padding(8.dp).padding(top = 10.dp)
+                        )
+
+                        IconButton(
+                            onClick = {
+                                falseNewMsg()
+                            },
+                            modifier = Modifier.size(30.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = "close",
+                                modifier = Modifier.size(30.dp)
+                            )
+                        }
+                    }
+
+                        Row (
+                            modifier = Modifier.fillMaxWidth().wrapContentWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            var searchUser by remember { mutableStateOf("") }
+                            OutlinedTextField(
+                                value = searchUser,
+                                onValueChange = {searchUser = it},
+                                placeholder = {
+                                    Text(text = "find user")
+                                },
+                                modifier = Modifier.weight(1f)
+                            )
+                            Spacer(Modifier.width(10.dp))
+                            IconButton(
+                                onClick = {},
+                                modifier = Modifier
+                                    .background(Color(57, 136, 227, 150), RoundedCornerShape(16.dp))
+                                    .clip(RoundedCornerShape(16.dp))
+                                    .height(45.dp)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.Search,
+                                    contentDescription = "findUser",
+                                    modifier = Modifier.fillMaxSize()
+                                )
+                            }
+                        }
+                    }
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun NewMessage(showNewMessage: () -> Unit, modifier: Modifier) {
     IconButton(
         onClick =  {
-
+            showNewMessage()
         },
         modifier = modifier
             .size(60.dp)
@@ -98,10 +197,11 @@ fun NewMessage(modifier: Modifier) {
             .clip(CircleShape),
     ) {
         Icon(
-            imageVector = Icons.Filled.MailOutline,
+            painter = painterResource(id = R.drawable.newmessage),
             contentDescription = "",
             modifier = Modifier
-                .size(40.dp)
+                .size(60.dp),
+            tint = Color.White
         )
     }
 }
@@ -254,12 +354,25 @@ fun TopAppBar(modifier: Modifier) {
             )
         }
         Spacer(modifier = Modifier.width(10.dp))
+
+        ThreeButtonDropdown()
+
+    }
+}
+
+@Composable
+fun ThreeButtonDropdown() {
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("profil", "settings")
+    var selectedOption by remember { mutableStateOf(options[0]) }
+
+    Box() {
         IconButton(
             modifier = Modifier
                 .background(Color(0, 0, 0, 150), CircleShape)
                 .size(40.dp),
             onClick = {
-
+                expanded = true
             }
         ) {
             Icon(
@@ -270,6 +383,23 @@ fun TopAppBar(modifier: Modifier) {
                     .size(35.dp)
 
             )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .background(Color(0xFF3988e3))
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(
+                    text = { Text(option) },
+                    onClick = {
+                        selectedOption = option
+                        expanded = false
+                    }
+                )
+            }
         }
     }
 }
